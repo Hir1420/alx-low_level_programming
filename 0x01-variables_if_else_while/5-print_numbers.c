bcf360f5aafaf9564ecb19751e567d8783f4de23
root <mingestuh@gmail.com>
2022-03-13 17:27:28 -0700

    don't you give up

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0 ; digit < 10; digit++)
+{
+printf("%ddigit", digit);
+}
+
+printf("\n");
+return (0);
+}
