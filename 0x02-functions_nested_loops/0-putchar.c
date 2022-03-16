d960c49faae97161ddb98b5d473c6072062d3ab2
HiraM <mingestuh@gmail.com>
2022-03-16 06:41:20 -0700

    first nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
+#include <main.h>
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - check the code
+ * main - entry
  *
- * Return: Always 0..
+ * Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char word[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
